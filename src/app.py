ea7d95489f3bae430de40ba2ff4f0fddd153855b
francistor <francisco.cardosogil@gmail.com>
2026-08-19 00:16:55 +0200

    local runner

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ def details():
 def health():
     return jsonify({
         "status": "up",
-        "weather": "fine :-))"
+        "weather": "fine :-)"
     }), 200
 
 if __name__ == "__main__":
