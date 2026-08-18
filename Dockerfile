829a65de78525078e8be5bdd9c806105e7fe5148
francistor <francisco.cardosogil@gmail.com>
2026-08-18 14:08:47 +0200

    initial push

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.10-alpine
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+COPY ./src /app
+ENTRYPOINT ["python", "/app/app.py"]
---
+++ src/app.py
@@ -0,0 +1,24 @@
+# /app/v1/details
+# /app/v1/healthz
+
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask("my big application")
+
+@app.route("/app/v1/details")
+def details():
+    return jsonify({
+        "time": datetime.datetime.now().strftime('%Y-%m-%d'),
+        "hostname": socket.gethostname()
+    })
+
+@app.route("/app/v1/healthz")
+def health():
+    return jsonify({
+        "status": "up"
+    }), 200
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
